add90272e648526a43a3c9d25c3d4839e3640bc9
alehxalves <alehxalves@gmail.com>
2025-12-01 23:14:13 -0300

    🧹 refactor(dashboard): simplify layout and remove unused pages
    Remove SidebarMenu and related state management from BoardLayout.
    Delete empty pages for 'priorizacao' and 'refinamento'.
    Refactor DashboardPage for better readability and performance.
    Improve NavBar styling and structure for better UX.

--- src/app/(private)/dashboard/board/[id]/layout.tsx
+++ src/app/(private)/dashboard/board/[id]/layout.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import UpdateBoard from "@/components/dashboard/board/UpdateBoard";
-import { SidebarMenu } from "@/components/dashboard/menu/SidebarMenu";
 import { NavBar } from "@/components/nav-bar";
 import { useBoard } from "@/hooks/useBoards";
-import { Box, Flex, useDisclosure, useMediaQuery } from "@chakra-ui/react";
+import { Flex, useDisclosure } from "@chakra-ui/react";
 import { useUser } from "@clerk/nextjs";
 import { useParams, useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 export default function BoardLayout({
   children,
@@ -19,8 +18,6 @@ export default function BoardLayout({
   const { board, loadBoard } = useBoard(params.id as string);
   const { isLoaded, isSignedIn } = useUser();
 
-  const [isMobile] = useMediaQuery(["(max-width: 1024px)"]);
-  const [isExpanded, setIsExpanded] = useState<boolean>(false);
   const { open, onOpen, onClose } = useDisclosure();
 
   useEffect(() => {
@@ -43,26 +40,7 @@ export default function BoardLayout({
         onEditBoard={onOpen}
       />
       <UpdateBoard isOpen={open} onClose={onClose} shouldRefetch={loadBoard} />
-      <Flex>
-        <SidebarMenu expanded={setIsExpanded} />
-        <Box
-          flex="1"
-          ml={
-            !isMobile
-              ? {
-                  base: "0",
-                  md: isExpanded ? "250px" : "40px",
-                  lg: isExpanded ? "250px" : "40px",
-                }
-              : "0px"
-          }
-          transition="margin-left 0.3s ease-in-out"
-          height="calc(100vh - 64px)"
-          overflowY="auto"
-        >
-          {children}
-        </Box>
-      </Flex>
+      <Flex>{children}</Flex>
     </>
   );
 }
